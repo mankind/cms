3941988c2771e34d48e2f31d32e32f24852056fd
edward <bauchiroad@gmail.com>
2015-01-31 15:33:04 +0000

    child pages

--- app/routes/page.js
+++ app/routes/page.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(params) {
+    
+     var pagesRouteModel = this.modelFor('pages');
+     var passedInValue = pagesRouteModel.findBy('name', params.page_name);
+     return passedInValue;
+
+  },
+  
+  serialize: function(model) {
+    return {page_name: model.get('name') };
+  } 
 });
--- app/routes/page/new-child-page.js
+++ app/routes/page/new-child-page.js
@@ -1,4 +1,64 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model: function(){
+    var pageParent = this.modelFor('page');
+   
+    var  newPage = this.store.createRecord('page' , {
+      parent: pageParent //, children: []
+    });
+    
+    //pageParent.get('children').pushObject(newPage);
+    
+    console.log('page-new-route', newPage);
+    return newPage;    
+  },
+  
+ setupController: function( controller, model) {
+     this._super();
+     controller.set('model', model);
+  },
+
+  actions: { 
+     
+     save: function(page){
+       //console.log('called save', page);
+       
+       var self = this;
+       //var pageParent = this.modelFor('page');
+       page.save().then(
+         function(data) {
+           
+           self.send('saveParent', data);
+           
+           self.transitionTo('page');
+         },
+         function(error) {
+          // work with person that failed to save
+           page.rollback();
+            //console.log("An error occured - Please try again", error);
+          }
+       ); 
+
+ 
+     },
+
+     saveParent: function(data){
+      var pageParent = this.modelFor('page');           
+      pageParent.get('children').pushObject(data);
+   
+      pageParent.save();
+           
+      //console.log('pageParent in new-child-route', pageParent);
+     },
+   
+     cancel: function(page) {
+       page.rollback();
+       this.transitionTo('page.index');   
+     } 
+   
+  } 
+  
+
 });
