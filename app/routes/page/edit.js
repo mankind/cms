fac923acdba264640dcc5219f604031d0048e1d3
edward <bauchiroad@gmail.com>
2015-01-31 15:19:41 +0000

    added page.hbs

--- app/routes/page/edit.js
+++ app/routes/page/edit.js
@@ -1,4 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(){
+    return this.modelFor('page');
+  },
+
+  actions: {   
+     save: function(page){
+       var self = this;
+      
+       page.save().then(
+         function() {
+             self.transitionTo('page', page);
+         },
+         function(error) {
+          // work with person that failed to save
+           self.get('currentModel').rollback();
+           //console.log("pages/new route error occured", error);
+          }
+       );   
+     },
+   
+    cancel: function(page) {
+      page.rollback();
+      this.transitionTo('page', page);   
+    } 
+   
+  } 
 });
